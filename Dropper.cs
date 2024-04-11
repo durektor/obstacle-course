e1a63d5102dd3eeac9b7ac07196d220064bdc73e
durektor <jim.beale@outlook.com>
2024-04-11 16:19:26 -0400

    Add files via upload

---
+++ Dropper.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+using UnityEngine.Analytics;
+
+public class Dropper : MonoBehaviour
+{
+    MeshRenderer meshRenderer;
+    Rigidbody rigidBody; 
+    [SerializeField] float timeToWait = 3f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        meshRenderer = GetComponent<MeshRenderer>();
+        rigidBody = GetComponent<Rigidbody>();
+        meshRenderer.enabled = false;
+        rigidBody.useGravity = false;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Time.time > timeToWait)
+        {
+            meshRenderer.enabled = true;
+            rigidBody.useGravity = true;
+            goto Next;
+        }
+        Next:
+        if (Time.time >= 4.5)
+        {
+            rigidBody.isKinematic = true;
+        }
+        
+    }
+}
---
+++ Mover.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mover : MonoBehaviour
+{
+    // SerializeField adds the value to the inspector in Unity
+    // [SerializeField] float xValue = 0f;
+    // [SerializeField] float yValue = 0f;
+    // [SerializeField] float zValue = 0f;
+   
+    // Start is called before the first frame update
+
+    [SerializeField] float moveSpeed = 10f;
+
+    
+
+    void Start()
+    {
+        PrintInstructions();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Move the 3D object Oscar .01 per frame along the x-axis
+        // the f tells the compiler that the number is a float
+        // transform.Translate(0.01f,0,0);
+
+        MovePlayer();
+    }
+
+    void PrintInstructions()
+    {
+        Debug.Log("Welcome to the game");
+        Debug.Log("Move your player with WASD or arrow keys");
+        Debug.Log("Don't hit the walls!");
+    }
+
+    void MovePlayer()
+    {
+        float xValue = Input.GetAxis("Horizontal") * Time.deltaTime * moveSpeed;
+        float zValue = Input.GetAxis("Vertical") * Time.deltaTime * moveSpeed;
+        transform.Translate(xValue,0,zValue);
+    }
+}
